08cefaecfc004707160b5afcb55f18a2fdb318d9
Eric Gustafson <weuxfya@gmail.com>
2019-11-15 21:39:32 -0700

    Create generateHtml function, write file to fs.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const inquirer = require('inquirer');
+const chalk = require('chalk');
+const boxen = require('boxen')
+const fs = require('fs')
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const teamArray = [];
+const generateHtml = require('./lib/generateHtml')
+const managerArray = [];
+const engineerArray = [];
+const internArray = [];
 let id = 0;
 let exit = false;
+const filename = './output/team.html'
 
 const questionsStart = [
     {
         type: "list",
-        message: "Select team member's role:",
+        message: ">>>>>>>Select team member's role:<<<<<<",
         name: "role",
         choices: ["Manager", "Engineer", "Intern", "Exit"]
     }
@@ -75,34 +82,43 @@ const inputStart = async () => {
 const collectInputs = async (role) => {
     id = id + 1
     if (role === 'Manager') {
+        console.log(boxen(chalk.white.bold(`Adding a new ${role} team member.`), { padding: 1, margin: 1, borderColor: 'green', backgroundColor: 'green' }))
         const questions = [...baseQuestions, ...managerQuestion];
         member = await inquirer.prompt(questions);
         const memberObj = new Manager(member.name, id, member.email, member.officeNumber);
-        teamArray.push(memberObj);
+        managerArray.push(memberObj);
     } else if (role === 'Engineer') {
+        console.log(boxen(chalk.white.bold(`Adding a new ${role} team member.`), { padding: 1, margin: 1, borderColor: 'green', backgroundColor: 'green' }))
         const questions = [...baseQuestions, ...engineerQuestion];
         member = await inquirer.prompt(questions)
         const memberObj = new Engineer(member.name, id, member.email, member.github);
-        teamArray.push(memberObj);
+        engineerArray.push(memberObj);
     } else if (role === 'Intern') {
+        console.log(boxen(chalk.white.bold(`Adding a new ${role} team member.`), { padding: 1, margin: 1, borderColor: 'green', backgroundColor: 'green' }))
         const questions = [...baseQuestions, ...internQuestion]
         member = await inquirer.prompt(questions)
         const memberObj = new Intern(member.name, id, member.email, member.school);
-        teamArray.push(memberObj);
+        internArray.push(memberObj);
     } else {
+        console.log(boxen(chalk.black.bold(`Exiting the Add Memeber CLI`), { padding: 1, margin: 1, borderColor: 'yellow', backgroundColor: 'yellow' }))
         return exit = true;
     }
 }
 
+const writeHtml = (filename, data) => {
+    fs.writeFile(filename, data, function (err) {
+        if (err) console.log(err);
+        console.log(chalk.green("File written successfully"));
+    });
+};
+
 const init = async () => {
     while (!exit) {
-        const role = await inputStart()
-        // const inputs = await collectInputs(role);
+        const role = await inputStart();
         await collectInputs(role);
     }
-    console.log(teamArray)
-
-
+    const html = await generateHtml(managerArray, engineerArray, internArray);
+    writeHtml(filename, html);
 }
 
 init();
---
+++ output/test.js
@@ -0,0 +1,42 @@
+const test = [
+  Manager {
+    name: 'Ted',
+    id: 1,
+    email: 'ted@email.com',
+    officeNumber: '134'
+  },
+  Engineer {
+    name: 'Kev',
+    id: 2,
+    email: 'kev@email.com',
+    github: 'kevgit'
+  },
+  Engineer {
+    name: 'Tim',
+    id: 3,
+    email: 'tim@email.com',
+    github: 'timmy'
+  },
+  Intern {
+    name: 'Kelly',
+    id: 4,
+    email: 'kelly@email.com',
+    school: 'MSU'
+  },
+  Engineer {
+    name: 'Eric',
+    id: 5,
+    email: 'eric@email.com',
+    github: 'eric-gustafaoon1'
+  },
+  Intern { name: 'Gavin', id: 6, email: 'gav@email.com', school: 'UM' },
+  Engineer {
+    name: 'Tom',
+    id: 7,
+    email: 'tom@email.com',
+    github: 'tommy123'
+  },
+  Intern { name: 'fred', id: 8, email: 'fred@email.com', school: 'GU' }
+]
+
+console.log(test)
